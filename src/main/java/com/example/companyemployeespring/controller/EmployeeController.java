c761fc2955e22bbaf5a56e97f23d5f63586e4d34
Gevorg Bashmaxchyan <gev.bashmaxchyan@mail.ru>
2022-03-08 14:29:59 +0400

    new

--- src/main/java/com/example/companyemployeespring/controller/EmployeeController.java
+++ src/main/java/com/example/companyemployeespring/controller/EmployeeController.java
@@ -5,12 +5,17 @@ import com.example.companyemployeespring.entity.Employee;
 import com.example.companyemployeespring.repository.CompanyRepository;
 import com.example.companyemployeespring.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -19,6 +24,8 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
     @Autowired
     private CompanyRepository companyRepository;
+    @Value("${companyemployeespring.upload.path}")
+    private String imagePath;
 
     @GetMapping("/allEmployee")
     public String allEmployees(ModelMap map) {
@@ -34,7 +41,13 @@ public class EmployeeController {
     }
 
     @PostMapping("/addEmployee")
-    public String addEmployee (@ModelAttribute Employee employee){
+    public String addEmployee (@ModelAttribute Employee employee, @RequestParam("picture")MultipartFile uplodedFile) throws IOException {
+        if (!uplodedFile.isEmpty()){
+            String fileName = System.currentTimeMillis() + "_" + uplodedFile.getOriginalFilename();
+            File newFile = new File(imagePath +fileName);
+            uplodedFile.transferTo(newFile);
+            employee.setPicUrl(fileName);
+        }
         employeeRepository.save(employee);
         return "redirect:/allEmployee";
     }
